dc3606490cb315ab67c7dd234da93e6dddc0e75a
Mariam Hassan <mh0729345@gmail.com>
2026-02-04 23:58:15 +0200

    update in infra1

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,7 @@
 # VPC
 #################################
 module "vpc" {
-  source = "./modules/vpc"
-
+  source               = "./modules/vpc"
   env                  = var.env
   vpc_cidr             = var.vpc_cidr
   public_subnet_count  = var.public_subnet_count
--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,15 +1,21 @@
 variable "env" {
-  type = string
+  type        = string
+  description = "Environment name"
 }
 
 variable "vpc_cidr" {
-  type = string
+  type        = string
+  description = "CIDR block for the VPC"
 }
 
 variable "public_subnet_count" {
-  type = number
+  type        = number
+  description = "Number of public subnets"
+  default     = 2
 }
 
 variable "private_subnet_count" {
-  type = number
+  type        = number
+  description = "Number of private subnets"
+  default     = 2
 }
