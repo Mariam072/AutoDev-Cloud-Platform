14d4f79a552a61a77023e7d03000091915ee01ed
Mariam Hassan <mh0729345@gmail.com>
2026-02-05 14:23:48 +0200

    edit infra with my aws account

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">=1.5.0"
   backend "s3" {
-    bucket = "terraform-state-mariam123"
+    bucket = "terraform-state-mariam1"
     key    = "global/s3/terraform.tfstate"
     region = "eu-west-1"
   }
