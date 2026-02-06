0ba6bcf783254895da9df5b8e6a8c577c6b94d92
Mariam Hassan <126279875+Mariam072@users.noreply.github.com>
2026-02-06 22:33:20 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,27 +12,47 @@ module "vpc" {
 #################################
 # EKS
 #################################
+
+data "aws_caller_identity" "current" {}
+
+module "iam" {
+  source         = "./modules/iam"
+  cluster_name   = var.cluster_name
+  env            = var.env
+  aws_account_id = data.aws_caller_identity.current.account_id
+}
+
 module "eks" {
   source = "./modules/eks"
 
-  env                        = var.env
-  cluster_name               = var.cluster_name
-  vpc_id                     = module.vpc.vpc_id
-  private_subnets             = module.vpc.private_subnet_ids
+  env              = var.env
+  cluster_name     = var.cluster_name
+  vpc_id           = module.vpc.vpc_id
+  private_subnets  = module.vpc.private_subnet_ids
 
-  node_group_instance_type   = var.node_group_instance_type
-  node_group_desired_size    = var.node_group_desired_size
-  node_group_min_size        = var.node_group_min_size
-  node_group_max_size        = var.node_group_max_size
+  cluster_role_arn = module.iam.cluster_role_arn
+  node_role_arn    = module.iam.node_role_arn
+
+  node_group_instance_type = var.node_group_instance_type
+  node_group_desired_size  = var.node_group_desired_size
+  node_group_min_size      = var.node_group_min_size
+  node_group_max_size      = var.node_group_max_size
 }
+
+module "irsa" {
+  source        = "./modules/irsa"
+  cluster_name  = var.cluster_name
+  oidc_issuer   = module.eks.cluster_oidc_issuer
+  env           = var.env
+}
+
 
 #################################
 # ECR
 #################################
 module "ecr" {
   source = "./modules/ecr"
-
-  env = var.env
+  env    = var.env
 }
 
 #################################
@@ -41,7 +61,20 @@ module "ecr" {
 module "cognito" {
   source = "./modules/cognito"
   env    = var.env
-  
+
+}
+
+#################################
+# NLB
+#################################
+module "nlb" {
+  source          = "./modules/nlb"
+  env             = var.env
+  vpc_id          = module.vpc.vpc_id
+  private_subnets = module.vpc.private_subnet_ids
+}
+
+
 }
 #################################
 # nlb
@@ -60,12 +93,12 @@ module "nlb" {
 module "api_gateway" {
   source = "./modules/api_gateway"
 
-  env                  = var.env
-  region               = var.aws_region
-  user_pool_id         = module.cognito.user_pool_id
-  user_pool_client_id  = module.cognito.user_pool_client_id
-  vpc_link_id          = module.api_gateway.vpc_link_id
-  nlb_listener_arn     = module.nlb.listener_arn
-  private_subnets      = module.vpc.private_subnet_ids
-  vpc_id               = module.vpc.vpc_id      
+  env                 = var.env
+  region              = var.aws_region
+  user_pool_id        = module.cognito.user_pool_id
+  user_pool_client_id = module.cognito.user_pool_client_id
+  nlb_listener_arn    = module.nlb.listener_arn
+  private_subnets     = module.vpc.private_subnet_ids
+  vpc_id              = module.vpc.vpc_id
+
 }
