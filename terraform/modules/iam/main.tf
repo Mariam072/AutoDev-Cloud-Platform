a67b84a7b22a33dc12b4654b1f5e74df82ca2d94
Mariam Hassan <126279875+Mariam072@users.noreply.github.com>
2026-02-07 01:36:11 +0200

    Update main.tf

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -58,17 +58,17 @@ resource "aws_iam_role_policy_attachment" "ecr_policy" {
 # Access Entry (IMPORTANT)
 ############################
 
-resource "aws_eks_access_entry" "admin_access" {
-  cluster_name  = var.cluster_name
-  principal_arn = "arn:aws:iam::${var.aws_account_id}:root"
-  type          = "STANDARD"
-}
+#resource "aws_eks_access_entry" "admin_access" {
+ # cluster_name  = var.cluster_name
+ # principal_arn = "arn:aws:iam::${var.aws_account_id}:root"
+ # type          = "STANDARD"
+#}
 
-resource "aws_eks_access_policy_association" "admin_policy" {
-  cluster_name  = var.cluster_name
-  principal_arn = "arn:aws:iam::${var.aws_account_id}:root"
-  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-  access_scope {
-    type = "cluster"
-  }
-}
+#resource "aws_eks_access_policy_association" "admin_policy" {
+#  cluster_name  = var.cluster_name
+#  principal_arn = "arn:aws:iam::${var.aws_account_id}:root"
+#  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+#  access_scope {
+#    type = "cluster"
+#  }
+#}
