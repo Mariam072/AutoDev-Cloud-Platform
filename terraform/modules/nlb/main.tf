9c70d231ece02870d5653d9b89c7adfe34a0c274
Mariam Hassan <126279875+Mariam072@users.noreply.github.com>
2026-02-08 21:03:32 +0200

    Update main.tf

--- terraform/modules/nlb/main.tf
+++ terraform/modules/nlb/main.tf
@@ -12,7 +12,7 @@ resource "aws_lb_target_group" "this" {
   port        = var.target_port
   protocol    = "TCP"
   vpc_id      = var.vpc_id
-  target_type = "ip"
+  target_type = "instance"
 
   health_check {
     protocol = "TCP"
