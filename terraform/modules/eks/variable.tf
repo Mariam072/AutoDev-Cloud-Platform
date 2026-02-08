765aec90ec01914e11e7df0e6d9899a8aa154cbe
Mariam Hassan <126279875+Mariam072@users.noreply.github.com>
2026-02-09 00:50:53 +0200

    Update variable.tf

--- terraform/modules/eks/variable.tf
+++ terraform/modules/eks/variable.tf
@@ -1,12 +1,12 @@
-variable "env" { type = string }
-variable "cluster_name" { type = string }
-variable "vpc_id" { type = string }
-variable "private_subnets" { type = list(string) }
-
-variable "node_group_instance_type" { type = string }
-variable "node_group_desired_size" { type = number }
-variable "node_group_min_size" { type = number }
-variable "node_group_max_size" { type = number }
-
-variable "cluster_role_arn" { type = string }
-variable "node_role_arn" { type = string }
+variable "env" {}
+variable "cluster_name" {}
+variable "vpc_id" {}
+variable "private_subnets" {}
+variable "node_group_instance_type" {}
+variable "node_group_desired_size" {}
+variable "node_group_min_size" {}
+variable "node_group_max_size" {}
+variable "cluster_role_arn" {}
+variable "node_role_arn" {}
+variable "target_port" {}
+variable "nlb_sg_id" {} # id of the NLB SG
