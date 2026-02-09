3237550058e005a73170a5f234495be1d7687a04
Mariam Hassan <126279875+Mariam072@users.noreply.github.com>
2026-02-09 21:22:19 +0200

    Update main.tf

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,3 +1,7 @@
+#################################
+# Security Groups
+#################################
+
 resource "aws_security_group" "eks_cluster_sg" {
   name   = "${var.cluster_name}-cluster-sg"
   vpc_id = var.vpc_id
@@ -8,15 +12,39 @@ resource "aws_security_group" "eks_node_sg" {
   vpc_id = var.vpc_id
 }
 
+# السماح للـ Nodes بالتواصل مع الـ Cluster (443)
+resource "aws_security_group_rule" "cluster_ingress_from_nodes" {
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_cluster_sg.id
+  source_security_group_id = aws_security_group.eks_node_sg.id
+}
+
+# السماح للـ Nodes بالخروج لأي مكان (مهم جدًا للـ bootstrap)
+resource "aws_security_group_rule" "node_egress_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  security_group_id = aws_security_group.eks_node_sg.id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
 # السماح للـ NLB بالوصول للـ NodePort
 resource "aws_security_group_rule" "allow_nodeport_from_vpc" {
-  type                     = "ingress"
-  from_port                = var.target_port
-  to_port                  = var.target_port
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.eks_node_sg.id
-  cidr_blocks= ["10.0.0.0/16"]
+  type              = "ingress"
+  from_port         = var.target_port
+  to_port           = var.target_port
+  protocol          = "tcp"
+  security_group_id = aws_security_group.eks_node_sg.id
+  cidr_blocks       = ["10.0.0.0/16"]
 }
+
+#################################
+# EKS Cluster
+#################################
 
 resource "aws_eks_cluster" "this" {
   name     = var.cluster_name
@@ -32,11 +60,17 @@ resource "aws_eks_cluster" "this" {
   }
 }
 
+#################################
+# EKS Managed Node Group
+#################################
+
 resource "aws_eks_node_group" "default" {
   cluster_name    = aws_eks_cluster.this.name
   node_group_name = "${var.env}-node-group"
   node_role_arn   = var.node_role_arn
   subnet_ids      = var.private_subnets
+
+  instance_types = [var.node_group_instance_type]
 
   scaling_config {
     desired_size = var.node_group_desired_size
@@ -44,21 +78,13 @@ resource "aws_eks_node_group" "default" {
     max_size     = var.node_group_max_size
   }
 
-  launch_template {
-    id      = aws_launch_template.node_lt.id
-    version = "$Latest"
+  # نربط الـ Node Group بالـ SG بتاعتنا
+  update_config {
+    max_unavailable = 1
   }
-}
 
-resource "aws_launch_template" "node_lt" {
-  name_prefix   = "${var.cluster_name}-lt"
- 
-  vpc_security_group_ids = [
-    aws_security_group.eks_node_sg.id
+  depends_on = [
+    aws_security_group_rule.cluster_ingress_from_nodes,
+    aws_security_group_rule.node_egress_all
   ]
 }
-
-
-
-
-
