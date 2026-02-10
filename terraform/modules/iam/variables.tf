dc43b40559b7af18ff6214d33ec582921133814e
Mariam Hassan <126279875+Mariam072@users.noreply.github.com>
2026-02-10 20:31:34 +0200

    Update variables.tf

--- terraform/modules/iam/variables.tf
+++ terraform/modules/iam/variables.tf
@@ -1,3 +1,13 @@
 variable "cluster_name" { type = string }
 variable "env" { type = string }
 #variable "aws_account_id" { type = string }
+variable "aws_region" {
+  type        = string
+  description = "AWS region"
+}
+
+variable "aws_account_id" {
+  type        = string
+  description = "AWS account ID"
+}
+
